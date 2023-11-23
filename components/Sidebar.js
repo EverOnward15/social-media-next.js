c22b2b825f6883f1d4b219ba428f44c6ced34f7b
EverOnward15 <leiste15@gmail,com>
2023-11-23 21:08:59 +0530

    added div around logout button

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -60,12 +60,14 @@ export default function Sidebar () {
                     <span className="hidden md:block">Notifications</span>
                 </Link>
                 <button className="w-full -my-2" onClick={handleSignOut} >
+                    <div className="overflow-hidden w-full">
                     <span className={pathname === "" ? activeElement : inactiveElement}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
                         </svg>
                         <span className="hidden md:block">Logout</span>
                     </span>
+                    </div>
                 </button>
             </div>
         </Card>
